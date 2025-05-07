b58704aad6375c99bf6908b06bb20b2015630b5d
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-05-07 13:51:55 +0100

    Initail commit

---
+++ Framework/Screens/CreateAPost.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export function CreateAPost() {
+    return (
+        <View>
+            <Text>CreateAPost</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
--- Framework/Screens/Login.js
+++ Framework/Screens/Login.js
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
+import { AppBotton } from "../Components/AppBotton";
 
-export function Login(params) {
+export function Login() {
     return (
-        < View>
-            <Text></Text>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>Sign In</Text>
+            <Text>Welcome back</Text>
+            <View>
+                <Text>Email</Text>
+                <TextInput
+                    placeholder="Enter your email"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+                <Text>Password</Text>
+                <TextInput
+                    placeholder="Enter your password"
+                    secureTextEntry={true}
+                />
+                <AppBotton>Login</AppBotton>
+            </View>
         </View>
     )
 }
