08ef779e4e519bac253cc94e5fbb26144cc24059
Miracle Obafemi <miracleobafemi09@gmail.com>
2025-05-14 13:53:25 +0100

    Carousle added to app

--- App.js
+++ App.js
@@ -1,7 +1,49 @@
+import { useEffect, useState, useCallback } from "react";
 import { StackNavigator } from "./Framework/Navigators/Stack";
-
+import {
+  Montserrat_100Thin, Montserrat_200ExtraLight, Montserrat_300Light, Montserrat_400Regular,
+  Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black
+} from "@expo-google-fonts/montserrat"
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({ Montserrat_100Thin });
+        await Font.loadAsync({ Montserrat_200ExtraLight });
+        await Font.loadAsync({ Montserrat_300Light });
+        await Font.loadAsync({ Montserrat_400Regular });
+        await Font.loadAsync({ Montserrat_500Medium });
+        await Font.loadAsync({ Montserrat_600SemiBold });
+        await Font.loadAsync({ Montserrat_700Bold });
+        await Font.loadAsync({ Montserrat_800ExtraBold });
+        await Font.loadAsync({ Montserrat_900Black });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+
   return (
     <StackNavigator />
   )
